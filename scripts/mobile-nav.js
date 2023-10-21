691141cd6c34380e65e48bc1eb166ae5e3e9ea47
Alexandre Lacroix <alexandrelacroix.35@gmail.com>
2023-10-21 20:11:53 +0200

    fix screen size issues

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -23,16 +23,12 @@ function toggleNav() {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     var menuButton = document.getElementById("menuButton");
-    //menuButton.style.display = 'none';
     menuButton.classList.add('opened');
     menuButton.setAttribute('aria-expanded', true);
 
     var sideNav = document.getElementById("sideNav");
     sideNav.classList.add('active');
     sideNav.style.width = "250px";
-
-    document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("main").style.marginRight = "-250px";
 
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 
@@ -43,16 +39,12 @@ function openNav() {
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
     var menuButton = document.getElementById("menuButton");
-    menuButton.style.display = 'inherit';
     menuButton.classList.remove('opened');
     menuButton.setAttribute('aria-expanded', false);
 
     var sideNav = document.getElementById("sideNav");
     sideNav.classList.remove('active');
     sideNav.style.width = "0";
-
-    document.getElementById("main").style.marginLeft = "0";
-    document.getElementById("main").style.marginRight = "0";
 
     document.body.style.backgroundColor = "white";
     
