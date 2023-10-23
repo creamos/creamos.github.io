f7b52382554455dcecc80d04d0c39488f9acb221
Alexandre Lacroix <alexandrelacroix.35@gmail.com>
2023-10-23 02:00:02 +0200

    cache loaded lang files

--- scripts/translation.js
+++ scripts/translation.js
@@ -1,15 +1,62 @@
+langTables = {};
+
 function Translate() {
     //initialization
     this.init = function (attribute, lng) {
         this.attribute = attribute;
         this.lng = lng;
     }
+
     //translate 
+    this.translateDocument = function () {
+        var allDom = document.getElementsByTagName("*");
+        for (var i = 0; i < allDom.length; i++) {
+            var elem = allDom[i];
+            var key = elem.getAttribute(_self.attribute);
+            if (key != null) {
+                //check if an attribute has to be set instead of the innerHTML
+                var attributeToSet = elem.getAttribute("lang-set-attr");
+                if (attributeToSet != null)
+                    elem.setAttribute(attributeToSet, langTables[this.lng][key]);
+                else
+                    elem.innerHTML = langTables[this.lng][key];
+            }
+        }
+    }
+
+    //fetch lang file if necessary then translate 
     this.process = function () {
         _self = this;
         var xrhFile = new XMLHttpRequest();
         //load content data 
-        xrhFile.open("GET", "lang/" + this.lng + ".json");
+        if (langTables[this.lng] != null) {
+            this.translateDocument();
+        } else {
+            xrhFile.open("GET", "lang/" + this.lng + ".json");
+            xrhFile.onreadystatechange = function () {
+                if (xrhFile.readyState === 4) {
+                    if (xrhFile.status === 200 || xrhFile.status == 0) {
+                        langTables[_self.lng] = JSON.parse(xrhFile.responseText);
+                        _self.translateDocument();
+                    }
+                }
+            }
+
+            xrhFile.send();
+        }
+    }
+
+    
+}
+
+function translate(lng, tagAttr) {
+    var translate = new Translate();
+    translate.init(tagAttr, lng);
+    translate.process();
+}
+
+/*
+xrhFile.open("GET", "lang/" + this.lng + ".json");
         xrhFile.onreadystatechange = function () {
             if (xrhFile.readyState === 4) {
                 if (xrhFile.status === 200 || xrhFile.status == 0) {
@@ -33,11 +80,5 @@ function Translate() {
         }
         
         xrhFile.send();
-    }
-}
 
-function translate(lng, tagAttr) {
-    var translate = new Translate();
-    translate.init(tagAttr, lng);
-    translate.process();
-}
+*/
