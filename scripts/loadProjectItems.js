dbfaea3b5395f6316f036a7bcddc563cde209e3d
Alexandre <alexandrelacroix.35@gmail.com>
2023-10-27 10:18:44 +0200

    update project items factory class

--- scripts/loadProjectItems.js
+++ scripts/loadProjectItems.js
@@ -8,7 +8,7 @@ function ProjectListFactory() {
         xrhFile.onreadystatechange = function () {
             if (xrhFile.readyState === 4) {
                 if (xrhFile.status === 200 || xrhFile.status == 0) {
-                    _self.createPanels(JSON.parse(JSON.minify(xrhFile.responseText)));
+                    _self.createPanels(JSON.parse(JSON.minify(xrhFile.responseText)), container);
                 }
             }
         }
@@ -16,9 +16,53 @@ function ProjectListFactory() {
         xrhFile.send();
     }
 
-    this.createPanels = function createPanels(projectsTable) {
-        projectsTable.forEach(projectEntry => {
-            console.log(projectEntry);
-        });
+    this.createPanels = function createPanels(projectsTable, container) {
+
+        function getEntryField(entry, fieldTag) {
+            var path = fieldTag.split(".");
+            var lastIndex = path.length-1;
+
+            var location = {...entry};
+            for (var i = 0; i < lastIndex; i++){
+                console.log("next step: ");
+                console.log(path[i]);
+                console.log(location);
+                location = {...location[path[i]]};
+            }
+
+            var result = location[path[lastIndex]];
+            console.log(result);
+            return result;
+        }
+
+        if ("content" in document.createElement("template")) {
+
+            const template = document.querySelector("template#project-item-template").content;
+
+
+            for (let index = 0; index < projectsTable.length; index++) {
+                console.log(projectsTable[index]);
+                const projectEntry = projectsTable[index];
+                container.appendChild(document.importNode(template, true))
+                const entry = container.children[container.children.length-1];
+
+                var fields = entry.getElementsByTagName("*");
+                for (var i = 0; i < fields.length; i++) {
+                    var field = fields[i];
+                    var key = field.getAttribute("entry-tag");
+                    if (key != null) {
+                        //check if an attribute has to be set instead of the innerHTML
+                        var attributeToSet = field.getAttribute("entry-set-attr");
+                        if (attributeToSet != null)
+                        field.setAttribute(attributeToSet, getEntryField(projectEntry, key));
+                        else
+                            field.innerHTML = getEntryField(projectEntry, key);
+                    }
+                }
+            }
+
+        }
+
+        return projectsTable;
     }
 }
